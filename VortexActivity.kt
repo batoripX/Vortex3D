432ecf43f509f49ebcbb8776791a9618564982ce
coderX <bbatoripers@gmail.com>
2026-06-08 15:58:08 +0400

    Update VortexActivity.kt

--- VortexActivity.kt
+++ VortexActivity.kt
@@ -1,23 +1,16 @@
 package com.vortex3d.app
-
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 
 class VortexActivity : Activity() {
+    companion object {
+        init {
+            System.loadLibrary("vortex3d_jni")
+        }
+    }
     private lateinit var surfaceView: VortexSurfaceView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        window.setDecorFitsSystemWindows(false)
-        window.insetsController?.let {
-            it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-            it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
-
         surfaceView = VortexSurfaceView(this)
         setContentView(surfaceView)
     }
