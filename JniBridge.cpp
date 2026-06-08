f8d158a190ff5d1803ebca3366090501dc4da5cb
coderX <bbatoripers@gmail.com>
2026-06-08 15:58:06 +0400

    Update JniBridge.cpp

--- JniBridge.cpp
+++ JniBridge.cpp
@@ -1,3 +1,4 @@
+
 #include <jni.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
@@ -7,23 +8,19 @@ import Vortex3D.Mesh;
 
 extern "C" {
 
-JNIEXPORT void JNICALL
-Java_com_vortex3d_app_NativeEngine_initVulkan(JNIEnv* env, jobject thiz, jobject surface) {
+JNIEXPORT void JNICALL Java_com_vortex3d_app_VortexActivity_initVulkan(JNIEnv* env, jobject thiz, jobject surface) {
     if (!surface) return;
-    
     ANativeWindow* window = ANativeWindow_fromSurface(env, surface);
     if (window) {
         // Core engine initialization handle goes here
     }
 }
 
-JNIEXPORT void JNICALL
-Java_com_vortex3d_app_NativeEngine_resizeDisplay(JNIEnv* env, jobject thiz, jint width, jint height) {
+JNIEXPORT void JNICALL Java_com_vortex3d_app_VortexActivity_resizeDisplay(JNIEnv* env, jobject thiz, jint width, jint height) {
     // Handle aspect ratio / swapchain changes
 }
 
-JNIEXPORT void JNICALL
-Java_com_vortex3d_app_NativeEngine_cleanupVulkan(JNIEnv* env, jobject thiz) {
+JNIEXPORT void JNICALL Java_com_vortex3d_app_VortexActivity_cleanupVulkan(JNIEnv* env, jobject thiz) {
     // Safe resource teardown
 }
 
