2e64dbbf1e7fc2b1ef1563455fa411f818412dae
coderX <bbatoripers@gmail.com>
2026-06-09 23:08:59 +0400

    Update VortexActivity.kt

--- VortexActivity.kt
+++ VortexActivity.kt
@@ -1,16 +1,14 @@
 package com.vortex3d.app
+
 import android.app.Activity
 import android.os.Bundle
 
 class VortexActivity : Activity() {
-    companion object {
-        init {
-            System.loadLibrary("vortex3d_jni")
-        }
-    }
     private lateinit var surfaceView: VortexSurfaceView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         surfaceView = VortexSurfaceView(this)
         setContentView(surfaceView)
     }
